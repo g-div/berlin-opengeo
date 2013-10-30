db0c601e168a5b3e20ccfc1821e13f2ddbc7c834
g-div <g-div@anche.no>
2013-10-30 20:19:55 +0100

    beautified

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,3 @@
-
 var path = require('path'),
     config = require(path.resolve(__dirname, '../../config.js')),
     mongojs = require('mongojs'),
@@ -14,7 +13,7 @@ exports.init = function (app) {
 
     app.get(config.api.url, function (req, res) {
 
-        db.collection('data').find(req.query, function(err, doc) {
+        db.collection('data').find(req.query, function (err, doc) {
             app.setResponse(res, doc);
         });
 
@@ -23,7 +22,7 @@ exports.init = function (app) {
     app.get(config.api.url + '/search/', function (req, res) {
         parseParams(req);
 
-        db.collection('data').find(req.query, function(err, doc) {
+        db.collection('data').find(req.query, function (err, doc) {
             app.setResponse(res, doc);
         });
 
--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-	"db": "geocoder",
-	"data": "data/full-data.json.stream",
-	"api": {
-		"hostname": "localhost",
-		"port": 9987,
+    "db": "geocoder",
+    "data": "data/full-data.json.stream",
+    "api": {
+        "hostname": "localhost",
+        "port": 9987,
         "url": "/api/v1/"
-	}
+    }
 };
