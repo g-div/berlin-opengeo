47f314604f1ac7f973d04f2cdd30aa487fc7d342
moklick <moklick@moklick.(none)>
2013-10-31 18:30:23 +0100

    parser.js bugfix

--- api/lib/parser.js
+++ api/lib/parser.js
@@ -4,18 +4,15 @@ String.prototype.trim = function(){
 
 module.exports.parseParams = function(query) {
 
-    var parseParams = function parseParams(query){
-        var street = query.replace(/\d/gi, ''),
-            housenr = /\d{1,3}/.exec(query),
-            plz = /\d{5}/.exec(query),
-            dbReq = {
-                name : street.trim() || '',
-                hausnr : typeof housenr !== undefined ? housenr[0].trim() : '',
-                plz : typeof plz !== undefined ? plz[0].trim() : ''
-            };
+    var street = query.replace(/\d/gi, ''),
+        housenr = /\d{1,3}/.exec(query),
+        plz = /\d{5}/.exec(query),
+        dbReq = {
+            name : street.trim() || '',
+            hausnr : typeof housenr !== undefined ? housenr[0].trim() : '',
+            plz : typeof plz !== undefined ? plz[0].trim() : ''
+        };
 
-        return dbReq;
-    }
-    
-    parseParams(query);
+    return dbReq;
+
 };
