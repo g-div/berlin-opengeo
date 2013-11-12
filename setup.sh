a10c22f579fc8d079ec5d815a04318928b59b854
g-div <g-div@anche.no>
2013-11-12 21:49:25 +0100

    setup.sh checks now for freespace

--- setup.sh
+++ setup.sh
@@ -19,6 +19,13 @@ cd $DEST
 
 # if your mongodb stores data in a folder with to few memory, please chose another one:
 #mongod --dbpath /lot/of/freespace
+FREE=$(df -h ./ | grep -vE '^Filesystem|tmpfs'| awk '{ print $4 }' | sed 's/G//')
+if [ $FREE -lt 500 ]; then
+	echo "There are less than 5 GB freespace"
+	echo "Please choose another mongopath:"
+	echo "mongod --dbpath /lot/of/freespace"
+fi
+
 mongoimport --db geocoder --collection data < data/full-data.json.stream
 
 echo "Clean the destination directory"
