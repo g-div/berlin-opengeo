f6ffbc59d6a2768f3f0d004b865078c5f04aa3a9
g-div <g-div@anche.no>
2013-12-16 00:33:05 +0100

    argv refactored...

--- cli/lib/commands/geocode.js
+++ cli/lib/commands/geocode.js
@@ -1,34 +1,37 @@
 var app,
-	db = require('../../../lib/db'),
-	flatiron = require('flatiron');
+	_ = require('underscore'),
+	flatiron = require('flatiron'),
+	YAML = require('js-yaml'),
+	path = require('path'),
+	fs = require('fs'),
+	db = require(path.resolve('./lib/db')),
+	config = require(path.resolve('./config.js')),
+	docs = path.resolve(config.documentation),
+	apiConfig = getApiDocumentation(),
+	argv = {};
+
+function getApiDocumentation() {
+    return YAML.safeLoad(fs.readFileSync(docs).toString());
+};
+
+function selectOperation(operations) {
+    return _.find(operations, function(oper) {
+        return oper.httpMethod === 'GET';
+    });
+};
+
+apiConfig.apis.forEach(function(api) {
+	selectOperation(api.operations).parameters.forEach(function(param) {
+		argv[param.name] = {
+				"description": param.description
+		};
+		argv[param.name][param.dataType] = true;
+	});
+});
 
 module.exports = function() {
 	app = this;
-	app.use(flatiron.plugins.cli, {
-
-		argv: {
-			verbose: {
-				alias: 'v',
-				description: 'print all errors',
-				boolean: true
-			},
-			hausnr: {
-				alias: 'hn',
-				description: 'define the number of the house',
-				string: true
-			},
-			name: {
-				alias: 'n',
-				description: 'define the name of the street',
-				string: true
-			},
-			plz : {
-				alias: 'p',
-				description: 'define the postal code of the address',
-				string: true
-			}
-		}
-	});
+	app.use(flatiron.plugins.cli, {argv: argv});
 
 	var query = {},
 		keys = Object.keys(this.argv).slice(1),
