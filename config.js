cc506a4a381689668de9b9218da9c4e86a4393b7
g-div <g-div@anche.no>
2013-12-16 00:33:22 +0100

    api-documentation moved

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "db": "geocoder",
-    "documentation": "./api/docs.yml",
+    "documentation": "./docs.yml",
     "api": {
         "hostname": "localhost",
         "port": 9987
