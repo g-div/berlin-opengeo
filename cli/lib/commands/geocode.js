ec166334bb05da165e635747a5c26f711a4093f8
g-div <g-div@anche.no>
2013-12-16 07:08:01 +0100

    parameters checking

--- cli/lib/commands/geocode.js
+++ cli/lib/commands/geocode.js
@@ -1,7 +1,10 @@
 var app,
 	flatiron = require('flatiron'),
 	path = require('path'),
-	db = require(path.resolve(__dirname, '../../../lib/db'));
+	_ = require('underscore'),
+	db = require(path.resolve(__dirname, '../../../lib/db')),
+	apitools = require(path.resolve(__dirname, '../../../lib/api-tools'));
+
 
 module.exports = function() {
 	app = this;
@@ -12,16 +15,22 @@ module.exports = function() {
 
 	keys.length = keys.length - 1;
 
-	keys.forEach(function(e, i) {
-		if (that.argv[e] == true || that.argv[e].length <= 0) {
-			app.log.error("Please insert a valid value for the following parameter:", e)
+	keys.forEach(function(argument, i) {
+		if (that.argv[argument] == true || that.argv[argument].length <= 0) {
+			app.log.error('Please insert a valid value for the following parameter:', argument)
 			process.exit();
 		}
-		query[e] = '' + that.argv[e];
+		if (_.contains(_.keys(apitools.getArgv()), argument)) {
+			query[argument] = '' + that.argv[argument];
+		} else {
+			app.log.error('Parameter unknow:', argument);
+			app.log.error('Please read the documentation or run "node cli/cli.js" (without any additional command) for help.')
+			process.exit()
+		}
 	});
 
 	if (keys.length == 0) {
-		app.log.error("Please pass at least one field.");
+		app.log.error('Please pass at least one field.');
 		process.exit();
 	}
 
