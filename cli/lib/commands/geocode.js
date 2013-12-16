8acebeb0fb93572ace7031113049ef56a19bf467
g-div <g-div@anche.no>
2013-12-16 02:10:33 +0100

    api-tools added

--- cli/lib/commands/geocode.js
+++ cli/lib/commands/geocode.js
@@ -1,51 +1,33 @@
 var app,
-	_ = require('underscore'),
 	flatiron = require('flatiron'),
-	YAML = require('js-yaml'),
 	path = require('path'),
-	fs = require('fs'),
-	db = require(path.resolve('./lib/db')),
-	config = require(path.resolve('./config.js')),
-	docs = path.resolve(config.documentation),
-	apiConfig = getApiDocumentation(),
-	argv = {};
-
-function getApiDocumentation() {
-    return YAML.safeLoad(fs.readFileSync(docs).toString());
-};
-
-function selectOperation(operations) {
-    return _.find(operations, function(oper) {
-        return oper.httpMethod === 'GET';
-    });
-};
-
-apiConfig.apis.forEach(function(api) {
-	selectOperation(api.operations).parameters.forEach(function(param) {
-		argv[param.name] = {
-				"description": param.description
-		};
-		argv[param.name][param.dataType] = true;
-	});
-});
+	db = require(path.resolve(__dirname, '../../../lib/db'));
 
 module.exports = function() {
 	app = this;
-	app.use(flatiron.plugins.cli, {argv: argv});
 
 	var query = {},
 		keys = Object.keys(this.argv).slice(1),
 		that = this;
 
-		keys.length = keys.length - 1;
-		
-		keys.forEach(function(e,i){
-			query[e] = '' + that.argv[e];
-		})
-	
-	db.searchQuery(query, function(data){
+	keys.length = keys.length - 1;
+
+	keys.forEach(function(e, i) {
+		if (that.argv[e] == true || that.argv[e].length <= 0) {
+			app.log.error("Please insert a valid value for the following parameter:", e)
+			process.exit();
+		}
+		query[e] = '' + that.argv[e];
+	});
+
+	if (keys.length == 0) {
+		app.log.error("Please pass at least one field.");
+		process.exit();
+	}
+
+	db.searchQuery(query, function(data) {
 		console.info(data);
 		process.exit();
 	});
-	
+
 };
