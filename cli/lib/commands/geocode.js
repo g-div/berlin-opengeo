024fad3dea1b85b0f530d408f7817e1209da4bfe
g-div <g-div@anche.no>
2013-12-16 07:45:57 +0100

    checks for missing parameters (already in docs.yml as required declared

--- cli/lib/commands/geocode.js
+++ cli/lib/commands/geocode.js
@@ -6,9 +6,23 @@ module.exports = function() {
 	app = this;
 
 	var query = {},
-		keys = Object.keys(this.argv).slice(1);
+		keys = Object.keys(this.argv).slice(1),
+		required = app.apitools.selectRequired(_.toArray(app.apitools.getArgv()));
 
 	keys.length = keys.length - 1;
+
+	required.forEach(function(requirement) {
+		if (!_.contains(keys, requirement)) {
+			app.log.error("Parameter missing:", requirement);
+			process.exit();
+		}
+	});
+
+	if (keys.length == 0) {
+		app.log.error('Please pass at least one field.');
+		app.log.help(app.showOptions());
+		process.exit();
+	}
 
 	keys.forEach(function(argument, i) {
 		if (app.argv[argument] == true || app.argv[argument].length <= 0) {
@@ -23,11 +37,6 @@ module.exports = function() {
 			process.exit()
 		}
 	});
-
-	if (keys.length == 0) {
-		app.log.error('Please pass at least one field.');
-		process.exit();
-	}
 
 	app.db.searchQuery(query, function(data) {
 		console.info(data);
